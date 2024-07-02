94a795b0c13c8349df9d4e534e2043063958a125
IvanaFerlin <ivana.ferlin@edu.fh-joanneum.at>
2024-07-02 17:28:14 +0200

    DrinkQueue added peek, size ans isEmpty

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -1,4 +1,35 @@
 package at.fhj.msd;
 
+import java.util.LinkedList;
+
+import java.util.NoSuchElementException;
+
+import java.util.Queue;
+
 public class DrinkQueue {
+
+
+    //Returns the first Drink object in the queue without removing it
+
+    public Drink peek() {
+
+        return drinks.peek();
+
+    }
+
+    // Returns the number of drinks in the queue
+
+    public int size() {
+
+        return drinks.size();
+
+    }
+
+    //Checks if the queue is empty
+
+    public boolean isEmpty() {
+
+        return drinks.isEmpty();
+
+    }
 }
