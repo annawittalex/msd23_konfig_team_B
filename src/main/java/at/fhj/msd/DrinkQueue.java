d1f18c98474697b67cd83a9efaec625fb42c4613
Paria Nikparsa <paria.nikparsa@edu.fh-joanneum.at>
2024-07-02 17:27:33 +0200

    implimmented new class DrinkQueue

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -1,4 +1,19 @@
 package at.fhj.msd;
 
 public class DrinkQueue {
+    //Adds a Drink object to the queue
+
+    public boolean offer(Drink drink) {
+
+        return drinks.offer(drink);
+
+    }
+
+    //Removes and returns the first Drink object in the queue
+
+    public Drink poll() {
+
+        return drinks.poll();
+
+    }
 }
