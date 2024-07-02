d2eafda437f900f5125ed109a597e182320eaf9e
Oleksandra Annawitt <oleksandra.annawitt@edu.fh-joanneum.at>
2024-07-02 17:27:25 +0200

    DrinkQueue modified

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -1,4 +1,30 @@
 package at.fhj.msd;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
 public class DrinkQueue {
+    private Queue<Drink> drinks;
+    public DrinkQueue() {
+        drinks = new LinkedList<>();
+    }
+    /**
+     * Removes and returns the first Drink object in the queue.
+     * @return the Drink object removed
+     * @throws NoSuchElementException if the queue is empty
+     */
+    public Drink remove() {
+        return drinks.remove();
+    }
+
+    /**
+     * Returns the first Drink object in the queue without removing it.
+     * @return the first Drink object
+     * @throws NoSuchElementException if the queue is empty
+     */
+    public Drink element() {
+        return drinks.element();
+    }
+
 }
